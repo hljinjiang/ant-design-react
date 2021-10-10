84e1b61577fedb3bc691af04bf928c54d271ecf0
ljl <776745393@qq.com>
2021-10-10 19:35:01 +0800

    fix(面包屑): bug

--- src/components/HeaderBreadcrumb/index.jsx
+++ src/components/HeaderBreadcrumb/index.jsx
@@ -6,8 +6,27 @@ import styles from './index.less';
 
 const HeaderBreadcrumb = (props) => {
   const route = props;
+
+  // 向头部添加dashboard
+  const handleAddDashBroad = () => {
+    if (!route.matchMenuKeys.includes('/dashboard')) {
+      return (
+        <Breadcrumb.Item key="/dashboard">
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref={`#${route.breadcrumb['/dashboard'].icon}`} />
+          </svg>
+          <Link to={'/dashboard'} className={styles.link}>
+            {route.breadcrumb['/dashboard'].name}
+          </Link>
+        </Breadcrumb.Item>
+      );
+    }
+    return undefined;
+  };
+
   return (
     <Breadcrumb>
+      {handleAddDashBroad()}
       {route.matchMenuKeys.map((item) => {
         return (
           <Breadcrumb.Item key={item}>
@@ -18,7 +37,7 @@ const HeaderBreadcrumb = (props) => {
                     {/* <svg className="icon" aria-hidden="true">
                       <use xlinkHref={`#${route.breadcrumb[bread].icon}`} />
                     </svg> */}
-                    <Link to={item} className={styles.link}>
+                    <Link to={route.matchMenuKeys.slice(-1)[0]} className={styles.link}>
                       {route.breadcrumb[bread].name}
                     </Link>
                   </React.Fragment>
